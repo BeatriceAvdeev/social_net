c75434c2b642f1925d625300dfded53ea228d2b9
BeatriceAvdeev <Beatriceavdeev10@gmail.com>
2025-06-15 18:27:27 +0300

    route7.2.6

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Messages from './components/Messages/Messages';
-import { BrowserRouter} from 'react-router-dom/cjs/react-router-dom.min';
-import { Route,Routes } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter,Routes,Route} from 'react-router';
 
 function App() {
   return (
     <div className='wrapper'>
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
         <Header />
         <NavBar />
         <div className='content'>
           <Routes>
+            <Route path="" element={<Profile />} />
             <Route path="profile" element={<Profile />} />
             <Route path="messages" element={<Messages />} />
           </Routes>
         </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   );
 }
