4ab086f0fbb7afd9df0a773ebac0038d502e70eb
BeatriceAvdeev <Beatriceavdeev10@gmail.com>
2025-06-22 18:56:53 +0300

    ref

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 import "./posts.css"
 import Post from "../Post/Post";
-
+let postText=React.createRef()
+let addPost = () => {
+    console.log(postText)
+}
 
 function Posts(props) {
     return (
 
         <div className="posts">
-            <input type="text" placeholder="enter the post" />
-            <button>add post</button>
+            <input type="text" ref={postText} placeholder="enter the post" />
+            <button onClick={addPost}>add post</button>
             {props.postsText.map(e => <Post message={e.message} name={e.name} id={e.id} />)}
         </div>
     )
