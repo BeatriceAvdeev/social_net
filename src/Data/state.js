117740a72a68fcbadc76546a92030500cb7beca8
BeatriceAvdeev <Beatriceavdeev10@gmail.com>
2025-07-27 18:18:17 +0300

    store bind

--- src/Data/state.js
+++ src/Data/state.js
@@ -1,70 +1,72 @@
+let store = {
+    _state: {
+        messagesPage: {
+            messagesText: [
+                { name: 'John Dow', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odit ducimus adipisci vitae earum odio dicta, veritatis quod quas numquam, corporis nostrum. Quaerat, in fuga error repudiandae sint impedit eveniet.', id: 1 },
+                { name: 'Ivan Ivanov', message: 'iam tierd', id: 2 },
+                { name: 'Petya', message: 'want a coffee', id: 3 }
+            ],
+            newMessageText: "",
+        },
 
-
-let state = {
-    messagesPage: {
-        messagesText: [
-            { name: 'John Dow', message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odit ducimus adipisci vitae earum odio dicta, veritatis quod quas numquam, corporis nostrum. Quaerat, in fuga error repudiandae sint impedit eveniet.', id: 1 },
-            { name: 'Ivan Ivanov', message: 'iam tierd', id: 2 },
-            { name: 'Petya', message: 'want a coffee', id: 3 }
-        ],
-        newMessageText: "",
+        profilePage: {
+            postsText: [
+                { name: 'John Dow', message: 'hello world', id: 1, likes: 7 },
+                { name: 'John Dow', message: 'i am cat', id: 2, likes: 3 },
+                { name: 'John Dow', message: 'lets sing', id: 3, likes: 4 }
+            ],
+            newPostText: "",
+        }
+    },
+    rerenderTree() {
+        console.log("its fake function");
     },
 
-    profilePage: {
-        postsText: [
-            { name: 'John Dow', message: 'hello world', id: 1, likes: 7 },
-            { name: 'John Dow', message: 'i am cat', id: 2, likes: 3 },
-            { name: 'John Dow', message: 'lets sing', id: 3, likes: 4 }
-        ],
-        newPostText: "",
+    subscribe(observer) {
+        this.rerenderTree = observer
+    },
+    addPost(postText) {
+        let postsText = this._state.profilePage.postsText
+        let newPost = {
+            name: "John Dow",
+            message: postText,
+            id: postsText.length + 1,
+            likes: 9
+        }
+        postsText.unshift(newPost)
+        this._state.profilePage.newPostText = ""
+        this.rerenderTree(this._state)
+    },
+    onPostChange(text) {
+        this._state.profilePage.newPostText = text
+        this.rerenderTree(this._state)
+    },
+    onMessageChange(text) {
+        this._state.messagesPage.newMessageText = text
+        this.rerenderTree(this._state)
+    },
+    addMessage(messageText) {
+        let messagesText = this._state.messagesPage.messagesText
+        let newMessage = {
+            name: "John Dow",
+            message: messageText,
+            id: messagesText.length + 1
+        }
+        messagesText.unshift(newMessage)
+        this._state.messagesPage.newMessageText = ""
+        this.rerenderTree(this._state)
+    },
+
+    getState(){
+        return this._state
     }
-
-
-}
-
-export default state;
-let rerenderTree = () => {
-    console.log("its fake function");
-}
-
-export let subscribe = (observer) => {
-    rerenderTree = observer
-}
-export let addPost = (postText) => {
-    let postsText = state.profilePage.postsText
-    let newPost = {
-        name: "John Dow",
-        message: postText,
-        id: postsText.length + 1,
-        likes: 9
-    }
-    postsText.unshift(newPost)
-    state.profilePage.newPostText = ""
-    rerenderTree(state)
-
-
-}
-export let onPostChange = (text) => {
-    state.profilePage.newPostText = text
-    rerenderTree(state)
-}
-
-export let onMessageChange = (text) => {
-    state.messagesPage.newMessageText = text
-    rerenderTree(state)
 }
 
 
-export let addMessage = (messageText) => {
-    let messagesText = state.messagesPage.messagesText
-    let newMessage = {
-        name: "John Dow",
-        message: messageText,
-        id: messagesText.length + 1
-    }
-    messagesText.unshift(newMessage)
-    state.messagesPage.newMessageText = ""
-    rerenderTree(state)
-}
 
-window.state = state
+
+export default store
+
+
+
+window.state = store._state
--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import state from "./Data/state";
+import store from "./Data/state";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import state from './Data/state';
-import { addPost, addMessage, onPostChange, onMessageChange, subscribe } from './Data/state';
+// import { addPost, addMessage, onPostChange, onMessageChange, subscribe } from './Data/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderTree = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} addMessage={addMessage} onPostChange={onPostChange} onMessageChange={onMessageChange} />
+      <App
+        state={state}
+        addPost={store.addPost.bind(store)} 
+        addMessage={store.addMessage.bind(store)} 
+        onPostChange={store.onPostChange.bind(store)} 
+        onMessageChange={store.onMessageChange.bind(store)} />
     </React.StrictMode>
   );
 }
 
-subscribe(rerenderTree)
+store.subscribe(rerenderTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-rerenderTree(state)
+rerenderTree(store.getState())
